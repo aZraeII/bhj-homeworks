4130bc027a4c2fdb3e82ce952b951ec99571a09d
aZraeII <kolya-@bk.ru>
2023-03-09 17:32:01 +0300

    lybrary

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,21 +1,21 @@
-const popup = document.getElementById('modal_main')
-const close = document.getElementsByClassName('modal__close_times')[0,1];
-const tap = popup.getElementsByClassName('show-success')[0];
-
-const popup1 = document.getElementById('modal_success')
-
-close.onclick = function(){
-  popup.className = 'modal';
-   popup1.className = 'modal';
-}
-
-tap.onclick = function (){
-  popup.className = 'modal';
-  popup1.className = 'modal modal_active';  
-}
+const popupRed = document.getElementById('modal_main')
+const popupGreen = document.getElementById('modal_success')
+const tap = document.getElementsByClassName('show-success')[0];
+let close = document.getElementsByClassName('modal__close');
 
 const loadPopup = function(){
-  popup.className = 'modal modal_active';
+  popupRed.className = 'modal modal_active';
 }
 setTimeout(loadPopup, 1000);
 
+for(let i = 0; i < close.length; i++){
+  close = document.getElementsByClassName('modal__close')[i];
+  close.onclick = function(){
+  popupRed.className = 'modal';
+  popupGreen.className = 'modal';
+  }
+}
+
+tap.onclick = function (){
+  popupGreen.className = 'modal modal_active';  
+}
