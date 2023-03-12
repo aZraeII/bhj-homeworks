2bbc56d32d13fe83f0d3a2a2f325263bd19ff1b5
aZraeII <kolya-@bk.ru>
2023-03-12 16:00:27 +0300

    на job

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -4,7 +4,7 @@ class Game {
     this.wordElement = container.querySelector('.word');
     this.winsElement = container.querySelector('.status__wins');
     this.lossElement = container.querySelector('.status__loss');
-
+    
     this.reset();
 
     this.registerEvents();
@@ -17,14 +17,13 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    document.addEventListener('keyup', event => {  
+      if (this.currentSymbol.textContent === event.key) {
+        this.success();
+      }else {
+        this.fail();
+      }
+    });
   }
 
   success() {
