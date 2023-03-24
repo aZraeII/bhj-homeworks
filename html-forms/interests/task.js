e72ffb714c2c8de081bbc89aff19aabf3f60ea18
aZraeII <kolya-@bk.ru>
2023-03-24 06:57:51 +0300

    job 2

---
+++ html-forms/interests/task.js
@@ -0,0 +1,18 @@
+const interests = document.querySelector('.interests_main');
+
+interests.addEventListener('change', event => {
+  const target = event.target.closest('.interest');
+  const childrens = target.querySelectorAll('.interests');
+  const parents = target.querySelector('.interest__check');
+
+  childrens.forEach(interests => {
+    const checkBoxes = interests.querySelectorAll('.interest__check');
+    checkBoxes.forEach(element => {
+      if (parents.checked) {
+        element.checked = true;
+      } else {
+        element.checked = false;
+      }
+    });
+  });
+});
