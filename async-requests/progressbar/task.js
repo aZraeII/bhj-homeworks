9d848d7f88a485360fb3a09c820ef926a774f513
aZraeII <kolya-@bk.ru>
2023-04-01 10:41:22 +0300

    готово1

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -4,7 +4,7 @@ const form = document.getElementById("form");
 form.addEventListener("submit", (e) => {
     const formData = new FormData(form);
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://netology-slow-rest.herokuapp.com/upload.php");
+    xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload");
 
     xhr.upload.onprogress = function(event) {
         progress.value = event.loaded / event.total;
